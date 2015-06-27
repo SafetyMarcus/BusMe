1b8d3b1ec03139bd80d769e768f8bf37fa43e23c
Marcus Hooper <hooperarma@gmail.com>
2015-06-27 23:33:31 +1000

    Updating launch screen

--- BusMe/BusMe/MainViewController.swift
+++ BusMe/BusMe/MainViewController.swift
@@ -34,44 +34,39 @@ class MainViewController: UIViewController, CLLocationManagerDelegate, MKMapView
         
         let priority = DISPATCH_QUEUE_PRIORITY_DEFAULT as Int
         let flag = 0 as UInt
-        
-//        dispatch_async(dispatch_get_global_queue(Int(QOS_CLASS_USER_INITIATED.value), 0))
-//        {
-            if let path = NSBundle.mainBundle().pathForResource("stop_times", ofType: "txt")
+
+        if let path = NSBundle.mainBundle().pathForResource("stop_times", ofType: "txt")
+        {
+            var stopTimesText = String(contentsOfFile: path, encoding: NSUTF8StringEncoding, error: nil)
+            var stopsArray = stopTimesText?.componentsSeparatedByString("\r\n")
+            
+            for index in 1...stopsArray!.count - 2
             {
-                var stopTimesText = String(contentsOfFile: path, encoding: NSUTF8StringEncoding, error: nil)
-                var stopsArray = stopTimesText?.componentsSeparatedByString("\r\n")
-                
-                for index in 1...stopsArray!.count - 2
+                if let value = stopsArray?[index]
                 {
-                    if let value = stopsArray?[index]
+                    var stopArray = value.componentsSeparatedByString(",")
+                    var day = stopArray[0]
+                    var time = stopArray[1]
+                    
+                    var stopString = stopArray[4] as NSString
+                    var stop = stopString.integerValue
+                    
+                    var stopId = stopArray[3]
+                    
+                    var existingIds: [String] = self.stops.allKeys as! [String]
+                    
+                    var stopsArray = [BusStop]()
+                    
+                    if contains(existingIds, stopId)
                     {
-                        var stopArray = value.componentsSeparatedByString(",")
-                        var day = stopArray[0]
-                        var time = stopArray[1]
-                        
-                        var stopString = stopArray[4] as NSString
-                        var stop = stopString.integerValue
-                        
-                        var stopId = stopArray[3]
-                        
-                        var existingIds: [String] = self.stops.allKeys as! [String]
-                        
-                        var stopsArray = [BusStop]()
-                        
-                        if contains(existingIds, stopId)
-                        {
-                            stopsArray = self.stops.objectForKey(stopId) as! [BusStop]
-                        }
-                        
-                        stopsArray.append(BusStop(day: day, time: time, stopNo: stop))
-                        self.stops.setObject(stopsArray, forKey: stopId)
+                        stopsArray = self.stops.objectForKey(stopId) as! [BusStop]
                     }
+                    
+                    stopsArray.append(BusStop(day: day, time: time, stopNo: stop))
+                    self.stops.setObject(stopsArray, forKey: stopId)
                 }
             }
-            
-            println(self.stops)
-//        }
+        }
     }
     
     func mapView(mapView: MKMapView!, didUpdateUserLocation userLocation: MKUserLocation!)
