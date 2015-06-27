3ba116ef892e2373e99c5ccf9ecc0f32c659c056
Marcus Hooper <hooperarma@gmail.com>
2015-06-27 14:35:55 +1000

    Initial commit
    Set up app. Added map

---
+++ BusMe/BusMe/MainViewController.swift
@@ -0,0 +1,18 @@
+//
+//  MainViewController.swift
+//  BusMe
+//
+//  Created by Marcus on 27/06/2015.
+//  Copyright (c) 2015 easygoingapps. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class MainViewController: UIViewController
+{
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+    }
+}
